240944dcb93a146c7f9972d64af26ce2db937e2a
Henrik Olsson <henriko491@gmail.com>
2013-07-02 16:42:19 +0200

    LOADUI-761 Removed loading of Swing classes

--- loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/JavaFXActivator.java
+++ loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/JavaFXActivator.java
@@ -16,7 +16,6 @@
 package com.eviware.loadui.ui.fx;
 
 import javafx.application.Platform;
-
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -25,72 +24,50 @@ public class JavaFXActivator implements BundleActivator
 	@Override
 	public void start( final BundleContext context ) throws Exception
 	{
-		System.out.println( "JavaFX2 bundle started!" );
+		System.out.println("JavaFX2 bundle started!");
 
 		final ClassLoader bundleClassLoader = JavaFXActivator.class.getClassLoader();
 
-		initJavaFXThreadWith( bundleClassLoader );
+		initJavaFXThreadWith(bundleClassLoader);
 
 	}
 
 	private void initJavaFXThreadWith( final ClassLoader bundleClassLoader )
 	{
-		Platform.runLater( new Runnable()
+		Platform.runLater(new Runnable()
 		{
 			@Override
 			public void run()
 			{
-				//This is needed for custom controls to be able to load their skins from this bundle.
-				//With multiple bundles this could be problematic, and should be replaced by some classloader that delegates to multiple bundles.
 
-				System.out
-						.println( "JavaFXActivator: loading Swing classes so they are visible by the licensing framework." );
-
-				// Instantiate objects to fix Classloading problems in tablelog. Do not remove.
-				//new org.jdesktop.swingx.JXTable();
-				new javax.swing.JScrollPane();
-				new javax.swing.JRadioButton();
-				new javax.swing.JTextField();
-				new javax.swing.JComboBox<String>();
-				new javax.swing.JToolTip();
-				new javax.swing.JOptionPane();
-				new javax.swing.JEditorPane();
-				new javax.swing.JProgressBar();
-				new javax.swing.JFileChooser();
-				new javax.swing.JToggleButton();
-				new javax.swing.JCheckBox();
-				new javax.swing.JRootPane();
-				new javax.swing.JFormattedTextField();
-
-				Thread.currentThread().setContextClassLoader( bundleClassLoader );
+				Thread.currentThread().setContextClassLoader(bundleClassLoader);
 
 			}
-		} );
+		});
 	}
 
 	@Override
 	public void stop( BundleContext context ) throws Exception
 	{
-		System.out.println( "JavaFX2 bundle stopped!" );
-		Thread thread = new Thread( new Runnable()
+		System.out.println("JavaFX2 bundle stopped!");
+		Thread thread = new Thread(new Runnable()
 		{
 			@Override
 			public void run()
 			{
 				try
 				{
-					Thread.sleep( 6000 );
-				}
-				catch( InterruptedException e )
+					Thread.sleep(6000);
+				} catch( InterruptedException e )
 				{
 					e.printStackTrace();
 				}
 
-				System.out.println( "Shutdown timed out, forcing close..." );
-				System.exit( 0 );
+				System.out.println("Shutdown timed out, forcing close...");
+				System.exit(0);
 			}
-		} );
-		thread.setDaemon( true );
+		});
+		thread.setDaemon(true);
 		thread.start();
 	}
 }
