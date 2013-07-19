6b49eeccb38c31afd5bc25c27326cd73b669af47
Renato Athaydes <renato.deathaydes@smartbear.com>
2013-07-19 11:15:45 +0200

    LOADUI-874 Improved TableLog test for high loads

--- loadui-fx-integration-tests/src/test/java/com/eviware/loadui/test/ui/fx/tablelog/TableLogTest.java
+++ loadui-fx-integration-tests/src/test/java/com/eviware/loadui/test/ui/fx/tablelog/TableLogTest.java
@@ -4,17 +4,12 @@ import com.eviware.loadui.test.TestState;
 import com.eviware.loadui.test.categories.IntegrationTest;
 import com.eviware.loadui.test.ui.fx.FxIntegrationTestBase;
 import com.eviware.loadui.test.ui.fx.states.ProjectLoadedWithoutAgentsState;
-import com.google.common.util.concurrent.SettableFuture;
-import javafx.application.Platform;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 import static com.eviware.loadui.test.ui.fx.tablelog.TableLogTestSupport.tableRows;
+import static com.eviware.loadui.test.ui.fx.tablelog.TableLogTestSupport.testRunStopsWithinLimit;
 import static com.eviware.loadui.ui.fx.util.test.LoadUiRobot.Component.FIXED_RATE_GENERATOR;
 import static com.eviware.loadui.ui.fx.util.test.LoadUiRobot.Component.TABLE_LOG;
 import static com.eviware.loadui.ui.fx.util.test.matchers.EmptyMatcher.empty;
@@ -22,7 +17,6 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 
 @Category( IntegrationTest.class )
 public class TableLogTest extends FxIntegrationTestBase
@@ -66,55 +60,17 @@ public class TableLogTest extends FxIntegrationTestBase
 		connect( FIXED_RATE_GENERATOR ).to( TABLE_LOG );
 
 		// AND
-		int veryHighLoad = 20000;
+		int veryHighLoad = 2000;
 		turnKnobIn( FIXED_RATE_GENERATOR ).to( veryHighLoad );
 
-		class ThreadHolder
-		{
-			Thread t;
-		}
-		final ThreadHolder holder = new ThreadHolder();
-
-		Platform.runLater( new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				holder.t = Thread.currentThread();
-			}
-		} );
+		long startT = System.currentTimeMillis();
 
 		// WHEN
 		runTestFor( 2, SECONDS );
 
 		// THEN
-		ensureJavaFXThreadIsNotLockedForMoreThan( 1, SECONDS, holder.t );
+		testRunStopsWithinLimit( startT, Math.round( veryHighLoad * 1.15 ) );
+
 	}
-
-	private void ensureJavaFXThreadIsNotLockedForMoreThan( int time, TimeUnit unit, Thread toKill )
-	{
-		final SettableFuture<Boolean> future = SettableFuture.create();
-		Platform.runLater( new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				future.set( true );
-			}
-		} );
-		try
-		{
-			System.out.println( "Making assertion now!!!!!!!!!!" );
-			assertThat( future.get( time, unit ), is( true ) );
-		}
-		catch( InterruptedException | TimeoutException | ExecutionException e )
-		{
-			System.out.println( "FAILED FAILED FAILED" );
-			toKill.interrupt();
-			fail( "Did not get back from javaFX Thread nicely, problem was: " + e );
-
-		}
-	}
-
 
 }
--- loadui-fx-integration-tests/src/test/java/com/eviware/loadui/test/ui/fx/tablelog/TableLogTestSupport.java
+++ loadui-fx-integration-tests/src/test/java/com/eviware/loadui/test/ui/fx/tablelog/TableLogTestSupport.java
@@ -5,11 +5,18 @@ import javafx.scene.Node;
 import java.util.Set;
 
 import static com.eviware.loadui.ui.fx.util.test.TestFX.findAll;
+import static org.junit.Assert.assertTrue;
 
-public class TableLogTestSupport
+class TableLogTestSupport
 {
-	public static Set<Node> tableRows()
+	static Set<Node> tableRows()
 	{
 		return findAll(".component-view .table-row-cell");
+	}
+
+
+	static void testRunStopsWithinLimit( long startT, long limit )
+	{
+		assertTrue( System.currentTimeMillis() - startT < limit );
 	}
 }
