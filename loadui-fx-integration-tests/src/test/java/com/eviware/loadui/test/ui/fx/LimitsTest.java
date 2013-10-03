0a80ed4e630da7d1d54ec9d5903e04c6f47dc65f
Saaran <sara.sannerfeldt@smartbear.com>
2013-10-03 10:51:43 +0200

    LOADUI-1179 Created limits test

---
+++ loadui-fx-integration-tests/src/test/java/com/eviware/loadui/test/ui/fx/LimitsTest.java
@@ -0,0 +1,86 @@
+package com.eviware.loadui.test.ui.fx;
+
+import com.eviware.loadui.api.model.ProjectItem;
+import com.eviware.loadui.test.TestState;
+import com.eviware.loadui.test.ui.fx.states.ProjectLoadedWithoutAgentsState;
+import com.eviware.loadui.test.ui.fx.states.SimpleWebTestState;
+import javafx.scene.input.KeyCode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sara
+ * Date: 2013-10-02
+ * Time: 13:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class LimitsTest extends FxIntegrationTestBase
+{
+	@Override
+	public TestState getStartingState()
+	{
+		return SimpleWebTestState.STATE;
+	}
+
+	@Test
+	public void shouldStopTestWhenRequestsReachLimit()
+	{
+		ProjectItem project = ProjectLoadedWithoutAgentsState.STATE.getProject();
+
+
+
+		click( "#set-limits" ).doubleClick( "#request-limit" ).type( "30" ).click( "#default" );
+
+		robot.clickPlayStopButton();
+
+		assertTrue( project.isRunning() );
+
+		waitForNode( "#abort-requests" );
+		waitForNodeToDisappear( "#abort-requests" );
+
+		assertTrue( !project.isRunning() );
+
+
+	}
+
+	@Test
+	public void shouldStopTestWhenTimeLimitIsReached() throws Exception
+	{
+		ProjectItem project = ProjectLoadedWithoutAgentsState.STATE.getProject();
+
+		click( "#set-limits" ).doubleClick( "#time-limit" ).type( "3" ).click( "#default" );
+
+		robot.clickPlayStopButton();
+
+		assertTrue( project.isRunning() );
+
+		sleep( 4000 );
+
+		assertTrue( !project.isRunning() );
+	}
+
+
+
+
+	@Test
+	public void shouldStopTestWhenFailuresReachLimit()
+	{
+		ProjectItem project = ProjectLoadedWithoutAgentsState.STATE.getProject();
+
+		click( ".component-view .text-field" ).type( "sdjsdhfsaasjhsdf" );
+
+		click( "#set-limits" ).doubleClick( "#failure-limit" ).type( "30" ).click( "#default" );
+
+		robot.clickPlayStopButton();
+
+		assertTrue( project.isRunning() );
+
+		waitForNode( "#abort-requests" );
+		waitForNodeToDisappear( "#abort-requests" );
+
+		assertTrue( !project.isRunning() );
+
+}
+}
