e4aaf5b067f3d0f871b8ff0c29b7c6930ab8df78
Renato Athaydes <renato.deathaydes@smartbear.com>
2013-10-11 11:24:34 +0200

    Added tempus-fugit library to int-tests. Improved IntTestUtilsTest using that.

--- loadui-integration-tests/src/test/java/com/eviware/loadui/test/IntegrationTestUtilsTest.java
+++ loadui-integration-tests/src/test/java/com/eviware/loadui/test/IntegrationTestUtilsTest.java
@@ -1,7 +1,9 @@
 package com.eviware.loadui.test;
 
+import com.google.code.tempusfugit.concurrency.RepeatingRule;
+import com.google.code.tempusfugit.concurrency.annotations.Repeating;
 import com.google.common.base.Function;
-import com.google.common.collect.Collections2;
+import org.junit.Rule;
 import org.junit.Test;
 
 import javax.annotation.Nullable;
@@ -9,8 +11,10 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Random;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static com.google.common.collect.Collections2.transform;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -19,6 +23,8 @@ import static org.mockito.Mockito.when;
  */
 public class IntegrationTestUtilsTest
 {
+	@Rule
+	public RepeatingRule rule = new RepeatingRule();
 
 	@Test
 	public void testNewestDirectoryIn_HappyPath()
@@ -40,7 +46,7 @@ public class IntegrationTestUtilsTest
 
 		File newest = IntegrationTestUtils.newestDirectoryIn( mockDir );
 
-		assertThat( newest, notNullValue() );
+		assertNotNull( newest );
 
 		for( File file : files )
 		{
@@ -49,46 +55,45 @@ public class IntegrationTestUtilsTest
 	}
 
 	@Test
+	@Repeating( repetition = 10 )
 	public void testNewestDirectoryIn_WithFilesAndDirectories()
 	{
+		System.out.println( "Running test" );
 		final int numberOfFilesToTestWith = 20;
 
-		for( int run = 0; run < 10; run++ )
+		File mockDir = mock( File.class );
+
+		Random random = new Random();
+		File[] files = new File[numberOfFilesToTestWith];
+		for( int i = 0; i < numberOfFilesToTestWith; i++ )
 		{
-			File mockDir = mock( File.class );
+			File file = mock( File.class );
+			when( file.lastModified() ).thenReturn( random.nextLong() );
+			when( file.isDirectory() ).thenReturn( random.nextBoolean() );
+			files[i] = file;
+		}
 
-			Random random = new Random();
-			File[] files = new File[numberOfFilesToTestWith];
-			for( int i = 0; i < numberOfFilesToTestWith; i++ )
+		assertThat( transform( Arrays.asList( files ), new Function<File, Boolean>()
+		{
+			@Nullable
+			@Override
+			public Boolean apply( @Nullable File input )
 			{
-				File file = mock( File.class );
-				when( file.lastModified() ).thenReturn( random.nextLong() );
-				when( file.isDirectory() ).thenReturn( random.nextBoolean() );
-				files[i] = file;
+				return input != null && input.isDirectory();
 			}
+		} ), not( contains( false ) ) );
 
-			assertThat( Collections2.transform( Arrays.asList( files ), new Function<File, Boolean>()
-			{
-				@Nullable
-				@Override
-				public Boolean apply( @Nullable File input )
-				{
-					return input != null && input.isDirectory();
-				}
-			} ), not( contains( false ) ) );
+		when( mockDir.listFiles() ).thenReturn( files );
 
-			when( mockDir.listFiles() ).thenReturn( files );
+		File newest = IntegrationTestUtils.newestDirectoryIn( mockDir );
 
-			File newest = IntegrationTestUtils.newestDirectoryIn( mockDir );
+		assertNotNull( newest );
+		assertThat( newest.isDirectory(), is( true ) );
 
-			assertThat( newest, notNullValue() );
-			assertThat( newest.isDirectory(), is( true ) );
-
-			for( File file : files )
-			{
-				if( file.isDirectory() )
-					assertThat( newest.lastModified(), greaterThanOrEqualTo( file.lastModified() ) );
-			}
+		for( File file : files )
+		{
+			if( file.isDirectory() )
+				assertThat( newest.lastModified(), greaterThanOrEqualTo( file.lastModified() ) );
 		}
 
 	}
