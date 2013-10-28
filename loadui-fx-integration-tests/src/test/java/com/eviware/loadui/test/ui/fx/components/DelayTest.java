1ca4de9844728b8d3a73cc68228bee89b51c845b
Saaran <sara.sannerfeldt@smartbear.com>
2013-10-28 13:19:05 +0100

    Test Delay Component logic

---
+++ loadui-fx-integration-tests/src/test/java/com/eviware/loadui/test/ui/fx/components/DelayTest.java
@@ -0,0 +1,66 @@
+package com.eviware.loadui.test.ui.fx.components;
+
+import com.eviware.loadui.test.TestState;
+import com.eviware.loadui.test.categories.IntegrationTest;
+import com.eviware.loadui.test.ui.fx.FxIntegrationTestBase;
+import com.eviware.loadui.test.ui.fx.states.ProjectLoadedWithoutAgentsState;
+import com.eviware.loadui.ui.fx.util.test.LoadUiRobot;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static com.eviware.loadui.test.matchers.EmptyMatcher.empty;
+import static com.eviware.loadui.test.ui.fx.tablelog.TableLogTestSupport.tableRows;
+import static com.eviware.loadui.ui.fx.util.test.LoadUiRobot.Component.*;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+import static org.loadui.testfx.Assertions.assertNodeExists;
+import static org.loadui.testfx.Assertions.verifyThat;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sara
+ * Date: 2013-10-24
+ * Time: 16:01
+ * To change this template use File | Settings | File Templates.
+ */
+@Category( IntegrationTest.class )
+public class DelayTest extends FxIntegrationTestBase
+{
+
+	@Override
+	public TestState getStartingState()
+	{
+		return ProjectLoadedWithoutAgentsState.STATE;
+	}
+
+	@After
+	public void teardown() throws Exception
+	{
+		ensureProjectIsNotRunning();
+		super.teardown();
+	}
+
+	@Test
+	public void TestDelay()
+	{
+		connect( FIXED_RATE_GENERATOR ).to( DELAY ).to( TABLE_LOG );
+
+		turnKnobIn( FIXED_RATE_GENERATOR ).to( 1 );
+		sleep( 1000 );
+		assertNodeExists( "1 / Sec" );
+
+		turnKnobIn( DELAY ).to( 3000 );
+
+		robot.clickPlayStopButton();
+
+		sleep( 7000 );
+		verifyThat( tableRows(), is( not( empty() ) ) );
+		robot.clickPlayStopButton();
+
+	}
+
+
+
+}
