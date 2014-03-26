9acd95dcc9f8c748e2dc9c3b0d1ce5af8617c870
Renato Athaydes <renato.deathaydes@smartbear.com>
2014-03-26 15:54:47 +0100

    SLT-469 Add HTTPS support to new web runner

--- loadui-project/loadui-web-runner/src/main/java/com/eviware/loadui/components/web/DefaultRequestRunnerProvider.java
+++ loadui-project/loadui-web-runner/src/main/java/com/eviware/loadui/components/web/DefaultRequestRunnerProvider.java
@@ -3,6 +3,7 @@ package com.eviware.loadui.components.web;
 import com.eviware.loadui.api.base.Clock;
 import com.eviware.loadui.api.component.ComponentContext;
 import com.eviware.loadui.components.web.api.RequestRunnerProvider;
+import com.eviware.loadui.components.web.internal.SocketFactoryProvider;
 import com.eviware.loadui.util.RealClock;
 import com.google.common.collect.Iterables;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -15,11 +16,12 @@ public class DefaultRequestRunnerProvider implements RequestRunnerProvider
 {
 	private final Clock clock = new RealClock();
 	private WebRunnerStatsSender statisticsSender;
+	private SocketFactoryProvider socketFactoryProvider = new SocketFactoryProvider();
 
 	public RequestRunner provideRequestRunner( ComponentContext context, URI pageUri, Iterable<URI> assetUris )
 	{
 		return new RequestRunner( clock,
-				HttpClientBuilder.create().build(),
+				HttpClientBuilder.create().setSSLSocketFactory( socketFactoryProvider.newSocketFactory() ) . build(),
 				Iterables.concat( asList( pageUri ), assetUris ),
 				createStatsSenderIfNecessary( context ) );
 	}
