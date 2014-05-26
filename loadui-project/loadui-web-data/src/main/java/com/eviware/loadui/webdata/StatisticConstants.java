9f50035f13eba7d131baeb91e9ea1e991c34d668
Henrik Olsson <henrik.olsson@smartbear.com>
2014-05-26 10:59:03 +0200

    SLT-616 changed forgotten string

--- loadui-project/loadui-web-data/src/main/java/com/eviware/loadui/webdata/StatisticConstants.java
+++ loadui-project/loadui-web-data/src/main/java/com/eviware/loadui/webdata/StatisticConstants.java
@@ -6,7 +6,7 @@ public interface StatisticConstants
 	public static final String TIMETAKEN_IDENTIFIER = "Time Taken";
 	public static final String RESPONSESIZE_IDENTIFIER = "Response Size";
 	public static final String FAILURE_IDENTIFIER = "Failure";
-	public static final String COMPLETED_IDENTIFIER = "Sent";
+	public static final String COMPLETED_IDENTIFIER = "Completed";
 
 
 }
